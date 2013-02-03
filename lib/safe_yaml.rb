858d60d6eae5e87ae95c4ab63980986acf031944
Zach Wily <zach@zwily.com>
2013-02-03 09:39:01 -0700

    Verify that Psych is actually being used on 1.9.x

--- lib/safe_yaml.rb
+++ lib/safe_yaml.rb
@@ -14,6 +14,18 @@ module SafeYAML
     :enable_symbol_parsing => false,
     :enable_arbitrary_object_deserialization => false
   }
+
+  def self.using_psych?
+    defined?(YAML::ENGINE) && YAML::ENGINE.yamler == 'psych'
+  end
+
+  def self.using_psych_1_9_3?
+    self.using_psych? && RUBY_VERSION >= "1.9.3"
+  end
+
+  def self.using_psych_1_9_2?
+    self.using_psych? && RUBY_VERSION == "1.9.2"
+  end
 end
 
 module YAML
@@ -21,7 +33,7 @@ module YAML
     safe_mode = safe_mode_from_options("load", options)
 
     arguments = [yaml]
-    if RUBY_VERSION >= "1.9.3"
+    if SafeYAML.using_psych_1_9_3?
       arguments << options[:filename]
     end
 
@@ -37,7 +49,7 @@ module YAML
     load_with_options(yaml, options.merge(:filename => filename))
   end
 
-  if RUBY_VERSION >= "1.9.3"
+  if SafeYAML.using_psych_1_9_3?
     require "safe_yaml/psych_handler"
     def self.safe_load(yaml, filename=nil)
       safe_handler = SafeYAML::PsychHandler.new
@@ -54,7 +66,7 @@ module YAML
       File.open(filename, 'r:bom|utf-8') { |f| self.unsafe_load f, filename }
     end
 
-  elsif RUBY_VERSION == "1.9.2"
+  elsif SafeYAML.using_psych_1_9_2?
     require "safe_yaml/psych_handler"
     def self.safe_load(yaml)
       safe_handler = SafeYAML::PsychHandler.new
@@ -92,7 +104,7 @@ module YAML
   class << self
     alias_method :unsafe_load, :load
 
-    if RUBY_VERSION >= "1.9.3"
+    if SafeYAML.using_psych_1_9_3?
       alias_method :load, :load_with_filename_and_options
     else
       alias_method :load, :load_with_options
